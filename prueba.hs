59cc1226a847238c976ab8b2da5f2071b41785fa
SaulAlejandroMamaniAlania <smamanialania@est.frba.utn.edu.ar>
2018-05-25 12:26:14 -0300

    replica 3 veces even

---
+++ prueba.hs
@@ -0,0 +1,10 @@
+import Text.Show.Functions
+
+funcion nivel=replicate 3 even
+
+f x y z
+       | (y 0) == z=map (fst.x z)
+       | otherwise=map (snd.x (y 0))
+
+
+funcion1 cond num lista str=(>str).sum.map (length.num).filter (lista cond)
